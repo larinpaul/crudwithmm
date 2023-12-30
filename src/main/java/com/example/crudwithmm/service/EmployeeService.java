51b81cbc327c8591d5912b2530a846e0fb1fff76
larinpaul <www.larinpavel@mail.ru>
2023-12-30 11:11:13 +0500

    Deleted some superfluous imports and comments

--- src/main/java/com/example/crudwithmm/service/EmployeeService.java
+++ src/main/java/com/example/crudwithmm/service/EmployeeService.java
@@ -1,9 +1,7 @@
 package com.example.crudwithmm.service;
 
 import com.example.crudwithmm.entity.Employee;
-import com.example.crudwithmm.exceptions.EmployeeNotFoundException;
 import com.example.crudwithmm.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,11 +18,6 @@ public class EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
-
-//    public Employee getEmployeeById(Long id) {
-//        return employeeRepository.findById(id)
-//                .orElseThrow(() -> new EmployeeNotFoundException("Employee with ID " + id + " not found"));
-//    }
 
     public Employee getEmployeeById(Long id) {
         return employeeRepository.findById(id).orElse(null);
