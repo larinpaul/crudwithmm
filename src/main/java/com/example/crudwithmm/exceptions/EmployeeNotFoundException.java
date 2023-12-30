9460c40915f9f6169b10523be0061bf2d3bb444a
larinpaul <www.larinpavel@mail.ru>
2023-12-30 12:42:36 +0500

    Updated EmployeeController, Employee and EmployeeService to be RESTful

--- src/main/java/com/example/crudwithmm/exceptions/EmployeeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudwithmm.exceptions;
-
-public class EmployeeNotFoundException extends RuntimeException {
-
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-
-}
